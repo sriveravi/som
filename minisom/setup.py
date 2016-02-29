1da5af970ad261427f47805ef8e97e877dae123e
Samuel Rivera <rivera.162@osu.edu>
2016-02-29 12:05:40 -0500

    removed cython.  wasn't much faster but easy to put back

--- minisom/setup.py
+++ minisom/setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-from Cython.Build import cythonize
+#from Cython.Build import cythonize
 
 setup(
   name='MiniSom',
@@ -13,5 +13,5 @@ setup(
   license="CC BY 3.0",
   py_modules=['minisom'],
   requires = ['numpy'],
-  ext_modules = cythonize("minisom.pyx"),
+#  ext_modules = cythonize("minisom.pyx"),
 )
